baa158b82b501ebd617bf2bb14c218e78903d32d
Finnbarr Ambrose <finnbarr.ambrose121@gmail.com>
2024-10-24 02:11:48 +0000

    lots of updates see comment
    Couldn't sleep so start working and realise by the end of this I hadn't saved as I went I have added:
     Income data def with input validation ,
    income data validation with try again,
    update income sheet def,
    main def with income input or quit option,
    imported datetime
    
    Main problems faced: getting the code to recognise date as date category as words amount as number and description as words but also optional
    
    main main problems: spelling and spaceing
    
    all so i seamed to have delted my creds.jaos file but add it in agein ??

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials 
+from datetime import datetime
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,8 +14,69 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("Budget Tracker")
 
-budget = SHEET.worksheet("Budget_Tracker_1_sheet")
+def get_income_data():
+    """
+    users income data
+    """
+    print("enter income data in the format: Date, Category, Amount, Description (optional).")
+    print("Example: 01/10/2024, Salary, 3000, Monthly base salary")
 
-data = budget.get_all_values()
 
-print(data)
+
+    while True:
+        data_str = input("Enter income here:\n")
+        data_list = [entry.strip() for entry in data_str.split(',')]
+
+        if validate_income_data(data_list):
+            print("Income data is valid!")
+            break
+        else:
+            print("Invalid input, please follow the format in Example ")
+        
+
+
+    return data_list
+    
+def validate_income_data(data):
+    """
+    Validate uesr income data
+    """
+    try:
+        if len(data) < 3:
+            raise ValueError("Incomplete data, please follow the format in Example ")
+
+        datetime.strptime(data[0], "%d/%m/%Y")
+        float(data[2])
+        
+    except ValueError as e:
+        print(f"Error: {e}")
+        return False
+    return True
+
+def update_income_worksheet(data):
+    """
+    add income data to Income worksheet
+    """
+    print("Updating Income worksheet...")
+    income_sheet = SHEET.worksheet("Income")
+    income_sheet.append_row(data)
+    print("Income worksheet updated")
+
+def main ():
+    """
+    main function to run the Budget Tracker options
+    """
+    while True:
+        action = input("Are you adding income data? Enter 'yes'/n if not Enter 'quit' to exit: ").lower()
+        if action == "yes":
+            income_data = get_income_data()
+            update_income_worksheet(income_data)
+        elif action == "quit":
+            print("go make some money before you try agein")
+            break
+        else:
+            print("invalid option try again")
+
+if __name__ == "__main__":
+    print("Welcome to the Budget Tracker")
+    main()
