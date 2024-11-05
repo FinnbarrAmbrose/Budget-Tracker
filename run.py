f88856eece9bdc04c2726693b6595d13fd7584c4
Finnbarr Ambrose <finnbarr.ambrose121@gmail.com>
2024-11-05 22:06:36 +0000

    addig in color
    I have added in colour to the terminal for invalid entries, report, Successful and unsuccessful uploads and the titles of the recent entries columns

--- run.py
+++ run.py
@@ -21,12 +21,12 @@ def get_financial_data(data_type):
     """
     Users income or expenses data.
     """
-    print(Fore.RED + f"Enter your {data_type} data below:")
+    print(f"Enter your {data_type} data below:")
 
-    date_input = input("Date (DD/MM/YYYY): ")
-    category_input = input("Category: ")
-    amount_input = input("Amount: ")
-    description_input = input("Description (optional): ")
+    date_input = input(Fore.MAGENTA + "Date (DD/MM/YYYY): ")
+    category_input = input(Fore.MAGENTA + "Category: ")
+    amount_input = input(Fore.MAGENTA + "Amount: ")
+    description_input = input(Fore.MAGENTA + "Description (optional): ")
 
     data_list = [date_input.strip(), category_input.strip(),
                  amount_input.strip(), description_input.strip()]
@@ -35,10 +35,10 @@ def get_financial_data(data_type):
         print(
             "Invalid input, please re-enter your data " 
             "following the correct format.")
-        date_input = input("Date (DD/MM/YYYY): ")
-        category_input = input("Category: ")
-        amount_input = input("Amount: ")
-        description_input = input("Description (optional): ")
+        date_input = input(Fore.MAGENTA + "Date (DD/MM/YYYY): ")
+        category_input = input(Fore.MAGENTA + "Category: ")
+        amount_input = input(Fore.MAGENTA + "Amount: ")
+        description_input = input(Fore.MAGENTA + "Description (optional): ")
         data_list = [date_input.strip(), category_input.strip(),
                      amount_input.strip(), description_input.strip()]
 
@@ -53,14 +53,14 @@ def validate_financial_data(data):
     try:
         if len(data) < 3 or not data[0] or not data[1] or not data[2]:
             raise ValueError(
-                "Missing required fields. Please ensure all fields are entered correctly.")
+               Fore.RED +  "Missing required fields. Please ensure all fields are entered correctly.")
 
         datetime.strptime(data[0], "%d/%m/%Y")
 
         float(data[2])
 
     except ValueError as e:
-        print(f"Validation error: {e}")
+        print(Fore.MAGENTA + f"Validation error: {e}")
         return False
 
     return True
@@ -74,9 +74,9 @@ def update_worksheet(data, worksheet_name):
         print(f"Updating {worksheet_name} worksheet...")
         worksheet = SHEET.worksheet(worksheet_name)
         worksheet.append_row(data)
-        print(f" {worksheet_name} worksheet updated")
+        print(Fore.GREEN + f" {worksheet_name} worksheet updated")
     except Exception as e:
-        print(f"{e} update Failed")
+        print(Fore.RED + f"{e} update Failed")
 
 
 def generate_financial_report():
@@ -94,13 +94,13 @@ def generate_financial_report():
 
         net_savings = total_income - total_expenses
 
-        print("\nFinancial Report\n")
-        print(f"Total Income: {total_income:.2f}")
-        print(f"Total Expenses: {total_expenses:.2f}")
-        print(f"Net Savings: {net_savings:.2f}\n")
+        print(Fore.BLUE + "\nFinancial Report\n")
+        print(Fore.BLUE + f"Total Income: {total_income:.2f}")
+        print(Fore.BLUE + f"Total Expenses: {total_expenses:.2f}")
+        print(Fore.BLUE + f"Net Savings: {net_savings:.2f}")
 
     except Exception as e:
-        print(f"Report failed to generate")
+        print(Fore.RED + f"Report failed to generate")
 
 
 def display_recent_entries(worksheet_name, num_entries=5):
@@ -114,14 +114,14 @@ def display_recent_entries(worksheet_name, num_entries=5):
         recent_entries = entries[:5]
 
         print(f"\nMost Recent {worksheet_name} Entries:\n")
-        print(f"{'Date':<12} {'Category':<20} {'Amount':<10} {'Description'}")
+        print(f"{Fore.BLUE + 'Date':<12} {Fore.BLUE + 'Category':<20} {Fore.BLUE + 'Amount':<10} {Fore.BLUE + 'Description'}")
 
         for entry in recent_entries:
             date, category, amount, description = entry
             print(f"{date:<12} {category:<20} {amount:<10} {description}")
 
     except Exception as e:
-        print(f"Failed to retrieve data from {worksheet_name} worksheet: {e}")
+        print(Fore.RED + f"Failed to retrieve data from {worksheet_name} worksheet: {e}")
 
 
 def main():
@@ -130,29 +130,28 @@ def main():
     """
     while True:
         action = input(
-            "WHaAre you adding income or expense data or do you what to "
-            "displaying recent entries, generate Report? "
-            "Enter 'income', 'expense', 'recent', 'report' if not enter 'quit' "
-            "to exit: ").lower()
+            "\nAre you adding data or do you what to display a total report or view the most recent entries \n" 
+            "For income enter 'Income' \n" "For expense enter 'Expense'\n" "For report enter 'Report'\n" "For recent enter 'Recent'\n"  
+            "if not enter 'quit' to exit\n" "\nEnter here:").lower()
         if action in ["income", "expense"]:
             financial_data = get_financial_data(action)
             worksheet_name = "Income" if action == "income" else "Expenses"
             update_worksheet(financial_data, worksheet_name)
         elif action == "recent":
             entry_type = input(
-                "Which entries do you want to see? Enter 'income' or 'expenses': ").lower()
+                "\nWhich entries do you want to see? Enter 'income' or 'expenses':").lower()
             display_recent_entries(
                 "Income" if entry_type == "income" else "Expenses")
         elif action == "report":
             generate_financial_report()
         elif action == "quit":
-            print("go make some money before you try agein")
+            print(Fore.CYAN + "Go make some money")
             break
         else:
-            print("invalid option try again")
+            print(Fore.RED + "invalid option try again")
 
 
 if __name__ == "__main__":
-    print("Welcome to the Budget Tracker")
+    print(Fore.WHITE +"\n Welcome to the Budget Tracker")
     main()
 
