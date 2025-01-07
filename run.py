eb1c0803d6360677775a47b43d94a52a1ae3fdf9
Finnbarr Ambrose <finnbarr.ambrose121@gmail.com>
2025-01-07 17:57:14 +0000

    Added Reference notes of videos to run.py

--- run.py
+++ run.py
@@ -4,6 +4,9 @@ from datetime import datetime
 from colorama import init, Fore
 init(autoreset=True)
 
+# Reference: Python Tutorial 7 - Formatting Output in Python
+# Link: https://www.youtube.com/watch?v=Ln5dzg820PU
+# This video helped me to make sure I formatted the programme in a structured way
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -31,6 +34,12 @@ def get_financial_data(data_type):
     data_list = [date_input.strip(), category_input.strip(),
                  amount_input.strip(), description_input.strip()]
 
+
+    # Reference: Python Tutorial: Using Try/Except Blocks for Error Handling
+    # Link: https://www.youtube.com/watch?v=NIWwJbo-9_8
+    # This video showed me how to prompt users to input correct valid inputs when incorrect inputs were used
+
+
     while not validate_financial_data(data_list):
         print(
             "Invalid input, please re-enter your data"
@@ -57,7 +66,9 @@ def validate_financial_data(data):
                 " Please ensure all fields are entered correctly.")
 
         datetime.strptime(data[0], "%d/%m/%Y")
-
+        # Reference: Advanced Exception Handling in Python
+        # Link: https://www.youtube.com/watch?v=ZUqGMDppEDs
+        # I was able to learn how to add a solid cheques for inputs the user made
         float(data[2])
 
     except ValueError as e:
@@ -78,6 +89,10 @@ def update_worksheet(data, worksheet_name):
         print(Fore.GREEN + f" {worksheet_name} worksheet updated")
     except Exception as e:
         print(Fore.RED + f"{e} update Failed")
+        # Reference: Python Tutorial: Using Try/Except Blocks for Error Handling
+        # Link: https://www.youtube.com/watch?v=NIWwJbo-9_8
+        # Help me add error messages that would tell the user what went wrong and how to fix it / use a valid input
+
 
 
 def generate_financial_report():
@@ -122,7 +137,9 @@ def display_recent_entries(worksheet_name, num_entries=5):
         for entry in recent_entries:
             date, category, amount, description = entry
             print(f"{date:<12} {category:<20} {amount:<10} {description}")
-
+        # Reference: Advanced Exception Handling in Python
+        # Link: https://www.youtube.com/watch?v=ZUqGMDppEDs
+        # Helped to understand the way in which I can tell the user that there is a problem in retrieving information from Google sheets
     except Exception as worksheet_name:
         print(
             Fore.RED + f"Failed to retrieve data from {worksheet_name}"
@@ -168,6 +185,9 @@ def main():
 
 
 if __name__ == "__main__":
+    # Reference: Python 101 Python Basics for Beginners Playlist
+    # Link: https://www.youtube.com/watch?v=JTFGllcJ29I&list=PLNnG2akFozlISSPF2UiUFSHieGCc_BA9b
+    # This video help me to understand Python better when using main() 
     print(Fore.WHITE + "\n   Welcome to the Budget Tracker")
     print(Fore.WHITE + "\nThe Budget Tracker will assist users in "
           "managing their finances by allowing them to record and monitor"
